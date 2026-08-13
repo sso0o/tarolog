8f449a4d9da00b535b056d2d0ca3b18f6ae2e24c
sychoi <ssssso.yyyy@gmail.com>
2026-08-13 16:09:23 +0900

    feat: 유료 빌드 모드(.env.paid)와 전면 광고 유닛 ID 추가

--- frontend/src/lib/admobConfig.ts
+++ frontend/src/lib/admobConfig.ts
@@ -1,2 +1,3 @@
 // src/lib/admobConfig.ts
 export const ADMOB_BANNER_AD_UNIT_ID = 'ca-app-pub-3940256099942544/6300978111'
+export const ADMOB_INTERSTITIAL_AD_UNIT_ID = 'ca-app-pub-3940256099942544/1033173712'
